1df61e658e754428e3ad3a90ac19e8d94692d357
Simon Lundström <simmel@soy.se>
2015-06-03 19:24:48 +0200

    Don't need a sub folder for the handler

--- lib/lita-wolframalpha.rb
+++ lib/lita-wolframalpha.rb
@@ -1,12 +1,10 @@
 require "lita"
 
-Lita.load_locales Dir[File.expand_path(
-  File.join("..", "..", "locales", "*.yml"), __FILE__
-)]
+module Lita
+  module Handlers
+    class Wolframalpha < Handler
+    end
 
-require "lita/handlers/wolframalpha"
-
-Lita::Handlers::Wolframalpha.template_root File.expand_path(
-  File.join("..", "..", "templates"),
- __FILE__
-)
+    Lita.register_handler(Wolframalpha)
+  end
+end
--- lib/lita/handlers/wolframalpha.rb
+++
@@ -1,8 +0,0 @@
-module Lita
-  module Handlers
-    class Wolframalpha < Handler
-    end
-
-    Lita.register_handler(Wolframalpha)
-  end
-end

e152402936eddacd9a2d37321631f69e9a386254
Simon Lundström <simmel@soy.se>
2015-06-03 19:34:39 +0200

    Add wolframalpha and it's route

--- lib/lita-wolframalpha.rb
+++ lib/lita-wolframalpha.rb
@@ -3,6 +3,13 @@ require "lita"
 module Lita
   module Handlers
     class Wolframalpha < Handler
+      route(/^w(?:olfram)?a(?:lpha)?\s+(.+)/, :wolframalpha, help: { "(wa|wolframalpha) the query to run" => "Query the WolframAlpha® computational knowledge engine™" })
+
+      def wolframalpha(response)
+        answer = response.match_data.captures.join
+        response.reply(answer)
+      end
+
     end
 
     Lita.register_handler(Wolframalpha)

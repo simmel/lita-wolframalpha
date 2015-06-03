087e14c558f1eee4f526d90cc98cbb6bf71df430
Simon Lundström <simmel@soy.se>
2015-06-03 19:15:45 +0200

    Add a first simple spec

--- spec/lita/handlers/wolframalpha_spec.rb
+++ spec/lita/handlers/wolframalpha_spec.rb
@@ -1,4 +1,7 @@
 require "spec_helper"
 
 describe Lita::Handlers::Wolframalpha, lita_handler: true do
+
+  it { is_expected.to route_command("wolframalpha director of movie pi").to(:wolframalpha) }
+
 end

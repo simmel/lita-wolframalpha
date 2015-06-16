3a2e6683adc6c386dc8e5c76b8037424adadc966
Simon Lundström <simmel@soy.se>
2015-06-16 13:03:18 +0200

    Add a real test against the WA API

--- spec/lita/handlers/wolframalpha_spec.rb
+++ spec/lita/handlers/wolframalpha_spec.rb
@@ -2,6 +2,15 @@ require "spec_helper"
 
 describe Lita::Handlers::Wolframalpha, lita_handler: true do
 
+  before do
+    require_relative '../../../lita_config'
+  end
+
   it { is_expected.to route_command("wolframalpha director of movie pi").to(:wolframalpha) }
+
+  it "director of movie pi" do
+    send_message("wolframalpha director of movie pi")
+    expect(replies.last).to eq("Darren Aronofsky")
+  end
 
 end
